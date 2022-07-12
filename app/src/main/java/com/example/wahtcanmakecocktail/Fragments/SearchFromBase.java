e3b6e8d2605c628c7ae03cfbc94d33b6247a7870
SEKI-YUTA <zard.since1991@gmail.com>
2022-07-12 17:24:00 +0900

    implement search from base frature

--- app/src/main/java/com/example/wahtcanmakecocktail/Fragments/SearchFromBase.java
+++ app/src/main/java/com/example/wahtcanmakecocktail/Fragments/SearchFromBase.java
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -22,13 +24,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SearchFromBase extends Fragment {
     JSONArray drinks;
     List<JSONObject> availableDrinks = new ArrayList<>();
     Spinner baseSpinner;
-    ImageButton imgBtn_filter;
     RecyclerView recycler_availableDrinks;
 
     ArrayAdapter<CharSequence> spinnerAdapter;
@@ -38,17 +40,9 @@ public class SearchFromBase extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_from_base, container, false);
-        drinks = MainActivity.getDrinks();
-        for(int i = 0; i < drinks.length(); i++) {
-            try {
-                availableDrinks.add(drinks.getJSONObject(i));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
+
 
         baseSpinner = view.findViewById(R.id.baseSpinner);
-        imgBtn_filter = view.findViewById(R.id.imgBtn_filter);
         recycler_availableDrinks = view.findViewById(R.id.recycler_availableDrinks);
 
         spinnerAdapter = ArrayAdapter.createFromResource(getContext(), R.array.alcohol_ingredient, android.R.layout.simple_spinner_item);
@@ -56,12 +50,40 @@ public class SearchFromBase extends Fragment {
         baseSpinner.setAdapter(spinnerAdapter);
 
 
-        AvailableDrinksAdapter adapter = new AvailableDrinksAdapter(getContext(), availableDrinks);
         recycler_availableDrinks.setHasFixedSize(true);
         recycler_availableDrinks.setLayoutManager(new GridLayoutManager(getContext(), 1));
-        recycler_availableDrinks.setAdapter(adapter);
 
+
+
+        baseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                availableDrinks.clear();
+                String base = getResources().getStringArray(R.array.alcohol_ingredient)[i];
+                Log.d("Base", base);
+                drinks = MainActivity.getDrinks();
+                for(int j = 0; j < drinks.length(); j++) {
+                    try {
+                        String itemBase = drinks.getJSONObject(j).getString("base");
+                        Log.d("Alcohol base", itemBase);
+                        if(base.equals(itemBase)) {
+                            availableDrinks.add(drinks.getJSONObject(j));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                AvailableDrinksAdapter adapter = new AvailableDrinksAdapter(getContext(), availableDrinks);
+                recycler_availableDrinks.setAdapter(adapter);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         return view;
     }
+
 }
